f4d333f9faec80c8028f4d948df5c2352db9a8e4
Rushi <189489rishi@gmail.com>
2022-02-15 19:04:08 +0530

    Added:Leap_year

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace CheckLeapYear
         {
             Console.WriteLine("Enter Year : ");
             int Year = int.Parse(Console.ReadLine());
-            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) 
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))  
                 Console.WriteLine("{0} is a Leap Year.", Year);
             else Console.WriteLine("{0} is not a Leap Year.", Year);
             Console.ReadLine();
