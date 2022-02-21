8d293b766d3ef393f94014d3760c4d3afb94c259
Rushi <189489rishi@gmail.com>
2022-02-21 11:07:39 +0530

    Added: Power of Two

--- Program.cs
+++ Program.cs
@@ -1,23 +1,30 @@
-﻿                  //Even or Odd Number
-using System;
+﻿using System;
 
 namespace Basic_Program
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter any number :- ");
-            int Num = Convert.ToInt32(Console.ReadLine());
-            if (Num % 2 == 0)
+
             {
-                Console.WriteLine("Enter number is even");
+                int power_2;
+                Console.WriteLine("Please enter value of n to print power of 2 table");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n >= 0 && n <= 30)
+                {
+                    for (int i = 0; i <= n; i++)
+                    {
+                        power_2 = Convert.ToInt32(Math.Pow(2, i));
+                        Console.WriteLine("2^" + i + " : " + power_2);
+                    }
+                }
+                else
+                    Console.WriteLine("Please Enter value of n from 0 to 30");
             }
-            else
-            {
-                Console.WriteLine("Enter number is odd");
-            }
-            Console.ReadLine();
         }
     }
 }
+
